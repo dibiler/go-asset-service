d27317c0bb14fc25757ff6ca312caaffbc2ca595
Pablo Leonor <pablo.leonor@ojd.es>
2026-05-07 12:02:48 +0200

    test: add 2 unit tests for asset service

--- internal/assets/service_test.go
+++ internal/assets/service_test.go
@@ -1,7 +1,32 @@
 package assets
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestFilterByType(t *testing.T) {
+	var assetData = []Asset{
+		{"1", "server-1", "server", "eu-west"}}
 
+	s := NewService(assetData)
+	testCorrectType := "server"
+	testWrongType := "database"
+	if len(s.FilterByType(testCorrectType)) != 1 {
+		t.Errorf("FilterByType did not find Type (%s)", testCorrectType)
+	}
+	if len(s.FilterByType(testWrongType)) != 0 {
+		t.Errorf("FilterByType found nonExisting Type (%s)", testWrongType)
+	}
+}
+
+func TestGetAll(t *testing.T) {
+	var assetData = []Asset{
+		{"1", "name-1", "type-1", "location-1"},
+		{"2", "name-2", "type-2", "location-2"},
+	}
+	s := NewService(assetData)
+
+	if len(s.GetAll()) != len(assetData) {
+		t.Error("GetAll does not have same length than assetData.")
+	}
 }
